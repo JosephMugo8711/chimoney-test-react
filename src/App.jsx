a9436ae29b5c45faa8f9f713972fc9a197aee05e
JosephMugo8711 <wamiti8711@gmail.com>
2024-10-02 13:12:02 +0300

    Feat: skeleton

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <div className="">
+      <h1>Welcome to React</h1>
+      <p>This is a simple React app to test chimoney typescript sdk.</p>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
